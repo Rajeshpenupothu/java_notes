32c57ee8a42f24159e273d329d1b506dd65fd836
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-09-14 00:29:12 +0530

    linear

---
+++ linear_search.java
@@ -0,0 +1,4 @@
+Linear search:A linear search is the simplest approach employed to search for an element in a data set.
+for ex a array wih elements [2,8,5,14,9] given and a number 14 is given to search in the array .
+-->the number 14 is at the index place 3.
+-->the number 14 will check from the array one by one ... this step will check one by one from starting of the array. 
